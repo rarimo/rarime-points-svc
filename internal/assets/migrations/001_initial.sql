2218ba84b637cc22307d718a009428e2e1e200a6
violog <51th.apprent1ce.f0rce@gmail.com>
2024-01-24 16:52:36 +0200

    Add indexes☝️ to events, make balance_id not nullable 🐞

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -12,7 +12,7 @@ CREATE TYPE event_status AS ENUM ('open', 'fulfilled', 'claimed');
 CREATE TABLE IF NOT EXISTS events
 (
     id            serial PRIMARY KEY,
-    balance_id    integer                     null REFERENCES balances (id),
+    balance_id integer not null REFERENCES balances (id),
     type          text                        not null,
     status        event_status                not null,
     created_at    timestamp without time zone not null default NOW(),
@@ -20,7 +20,12 @@ CREATE TABLE IF NOT EXISTS events
     points_amount integer
 );
 
+CREATE INDEX IF NOT EXISTS events_balance_id_index ON events using btree (balance_id);
+CREATE INDEX IF NOT EXISTS events_type_index ON events using btree (type);
+
 -- +migrate Down
+DROP INDEX IF EXISTS events_type_index;
+DROP INDEX IF EXISTS events_balance_id_index;
 DROP TABLE IF EXISTS events;
 DROP TYPE IF EXISTS event_status;
 DROP TABLE IF EXISTS balances;
