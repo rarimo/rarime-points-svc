354e734f41e0e56ad3039691a0dfbc6bb3b919fd
violog <51th.apprent1ce.f0rce@gmail.com>
2024-01-29 14:44:17 +0200

    Replace dates with Unix time to allow seamless pagination

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,14 +1,14 @@
 -- +migrate Up
 CREATE OR REPLACE FUNCTION trigger_set_updated_at() RETURNS trigger
     LANGUAGE plpgsql
-AS $$ BEGIN NEW.updated_at = NOW() at time zone 'utc'; RETURN NEW; END; $$;
+AS $$ BEGIN NEW.updated_at = EXTRACT('EPOCH' FROM NOW()); RETURN NEW; END; $$;
 
 CREATE TABLE IF NOT EXISTS balances
 (
     did        text PRIMARY KEY,
-    amount     integer                     not null default 0,
-    created_at timestamp without time zone not null default NOW(),
-    updated_at timestamp without time zone not null default NOW()
+    amount     integer not null default 0,
+    created_at integer not null default EXTRACT('EPOCH' FROM NOW()),
+    updated_at integer not null default EXTRACT('EPOCH' FROM NOW())
 );
 
 CREATE INDEX IF NOT EXISTS balances_amount_index ON balances using btree (amount);
@@ -23,18 +23,19 @@ CREATE TYPE event_status AS ENUM ('open', 'fulfilled', 'claimed');
 
 CREATE TABLE IF NOT EXISTS events
 (
-    id            uuid PRIMARY KEY            not null default gen_random_uuid(),
-    user_did      text                        not null REFERENCES balances (did),
-    type          text                        not null,
-    status        event_status                not null,
-    created_at    timestamp without time zone not null default NOW(),
-    updated_at    timestamp without time zone not null default NOW(),
+    id            uuid PRIMARY KEY not null default gen_random_uuid(),
+    user_did      text             not null REFERENCES balances (did),
+    type          text             not null,
+    status        event_status     not null,
+    created_at    integer          not null default EXTRACT('EPOCH' FROM NOW()),
+    updated_at    integer          not null default EXTRACT('EPOCH' FROM NOW()),
     meta          jsonb,
     points_amount integer
 );
 
 CREATE INDEX IF NOT EXISTS events_user_did_index ON events using btree (user_did);
 CREATE INDEX IF NOT EXISTS events_type_index ON events using btree (type);
+CREATE INDEX IF NOT EXISTS events_updated_at_index ON events using btree (updated_at);
 
 CREATE TRIGGER set_updated_at
     BEFORE UPDATE
@@ -46,6 +47,7 @@ EXECUTE FUNCTION trigger_set_updated_at();
 DROP TRIGGER IF EXISTS set_updated_at ON events;
 DROP INDEX IF EXISTS events_type_index;
 DROP INDEX IF EXISTS events_user_did_index;
+DROP INDEX IF EXISTS events_updated_at_index;
 DROP TABLE IF EXISTS events;
 DROP TYPE IF EXISTS event_status;
 
