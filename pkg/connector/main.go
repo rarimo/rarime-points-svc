0b42bcd7672be9f57332af8aee485daa48d39869
violog <51th.apprent1ce.f0rce@gmail.com>
2024-02-22 12:57:39 +0200

    Drop ignoring errors functionality from connector

--- pkg/connector/main.go
+++ pkg/connector/main.go
@@ -16,22 +16,11 @@ import (
 const privatePrefix = "/integrations/rarime-points-svc/v1/private"
 
 type Client struct {
-	conn    *conn.Connector
-	ignored []ErrorCode
+	conn *conn.Connector
 }
 
 func NewClient(cli iface.Client) *Client {
 	return &Client{conn: conn.NewConnector(cli)}
-}
-
-// IgnoreErrors creates a client copy which return nil error on the specified
-// error codes. It simplifies error handling when you want to consider some cases
-// a valid behaviour.
-func (c *Client) IgnoreErrors(codes ...ErrorCode) *Client {
-	return &Client{
-		conn:    c.conn,
-		ignored: codes,
-	}
 }
 
 func (c *Client) FulfillEvent(ctx context.Context, req FulfillEventRequest) *Error {
@@ -49,9 +38,6 @@ func (c *Client) FulfillEvent(ctx context.Context, req FulfillEventRequest) *Err
 			err: fmt.Errorf("failed to extract error code: %w; base error: %w", err, baseErr),
 		}
 	}
-	if c.isIgnored(code) {
-		return nil
-	}
 
 	return &Error{
 		Code: code,
@@ -62,15 +48,6 @@ func (c *Client) FulfillEvent(ctx context.Context, req FulfillEventRequest) *Err
 func (c *Client) VerifyPassport(ctx context.Context, req VerifyPassportRequest) error {
 	u, _ := url.Parse(privatePrefix + "/balances")
 	return c.conn.PatchJSON(u, req, ctx, nil)
-}
-
-func (c *Client) isIgnored(code ErrorCode) bool {
-	for _, ig := range c.ignored {
-		if ig == code {
-			return true
-		}
-	}
-	return false
 }
 
 func extractErrCode(err error) (ErrorCode, error) {
