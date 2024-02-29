e82541ff549a3480432313ddc4b3c263a9fce48f
violog <51th.apprent1ce.f0rce@gmail.com>
2024-02-29 12:33:23 +0200

    Index-based referral code derivation

--- internal/service/referralid/main.go
+++ internal/service/referralid/main.go
@@ -2,17 +2,30 @@ package referralid
 
 import (
 	"crypto/sha256"
+	"fmt"
 	"math/big"
 )
 
-const bytesCount = 6
+const bytesCount = 8
 
-// New returns a base62 of the first 6 bytes of SHA-256 of the input
-func New(s string) string {
+// New generates a new deterministic referral ID from DID and index.
+func New(did string, index uint) string {
+	s := fmt.Sprintf("%s_%d", did, index)
 	hash := sha256.New()
 	hash.Write([]byte(s))
 	first := hash.Sum(nil)[:bytesCount]
 	return base62Encode(first)
+}
+
+// NewMany generates a bunch of referral IDs for a single DID with incrementing
+// index. Specify non-zero index argument to start from a specific index, this is
+// useful when you have stored referral IDs for this DID previously.
+func NewMany(did string, count, index uint) []string {
+	ids := make([]string, count)
+	for i := index; index < count; index++ {
+		ids[i] = New(did, i)
+	}
+	return ids
 }
 
 // using alphabet: [0-9a-zA-Z]
