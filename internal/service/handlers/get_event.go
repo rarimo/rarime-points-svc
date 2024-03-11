b7cd17dd3ccaed0486a2a36d8266da928bab355f
violog <51th.apprent1ce.f0rce@gmail.com>
2024-03-11 10:38:29 +0200

    Filter inactive types on getting event too

--- internal/service/handlers/get_event.go
+++ internal/service/handlers/get_event.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/rarimo/auth-svc/pkg/auth"
+	"github.com/rarimo/rarime-points-svc/internal/data/evtypes"
 	"github.com/rarimo/rarime-points-svc/internal/service/requests"
 	"github.com/rarimo/rarime-points-svc/resources"
 	"gitlab.com/distributed_lab/ape"
@@ -29,10 +30,17 @@ func GetEvent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	evType := EventTypes(r).Get(event.Type, evtypes.FilterInactive)
+	if evType == nil {
+		Log(r).Debugf("Event type is not active at this moment: %s", event.Type)
+		ape.RenderErr(w, problems.NotFound())
+		return
+	}
+
 	if !auth.Authenticates(UserClaims(r), auth.UserGrant(event.UserDID)) {
 		ape.RenderErr(w, problems.Unauthorized())
 		return
 	}
 
-	ape.Render(w, resources.EventResponse{Data: newEventModel(*event, EventTypes(r).Get(event.Type).Resource())})
+	ape.Render(w, resources.EventResponse{Data: newEventModel(*event, evType.Resource())})
 }
