6353f4b58dfb84b916ec3fa8e61b2d9d4280bd97
Zaptoss <zaptosgg@gmail.com>
2024-03-11 17:27:41 +0200

    Remove unnecessary request in DB in verify_passport

--- internal/service/handlers/verify_passport.go
+++ internal/service/handlers/verify_passport.go
@@ -29,32 +29,7 @@ func VerifyPassport(w http.ResponseWriter, r *http.Request) {
 
 	expireDate := time.Now().UTC().AddDate(0, 1, 0)
 
-	balance, err := BalancesQ(r).FilterByPassportHash(req.Hash).Get()
-	if err != nil {
-		log.WithError(err).Error("Failed to get balance by DID")
-		ape.RenderErr(w, problems.InternalError())
-		return
-	}
-
-	if balance != nil {
-		if balance.DID != req.UserDID {
-			log.Error("passport_hash already in use")
-			ape.RenderErr(w, problems.Conflict())
-			return
-		}
-
-		err = BalancesQ(r).FilterByDID(req.UserDID).SetPassport(balance.PassportHash.String, expireDate)
-		if err != nil {
-			log.WithError(err).Error("Failed to set expiration date")
-			ape.RenderErr(w, problems.InternalError())
-			return
-		}
-
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	balance, err = BalancesQ(r).FilterByDID(req.UserDID).Get()
+	balance, err := BalancesQ(r).FilterByDID(req.UserDID).Get()
 	if err != nil {
 		log.WithError(err).Error("Failed to get balance by DID")
 		ape.RenderErr(w, problems.InternalError())
@@ -113,65 +88,83 @@ func VerifyPassport(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = EventsQ(r).Transaction(func() error {
-		// If you make this endpoint public, you should check the passport hash for
-		// uniqueness and provide a better validation. Think about other changes too.
-		err = BalancesQ(r).FilterByDID(req.UserDID).SetPassport(req.Hash, expireDate)
-		if err != nil {
-			return fmt.Errorf("set passport for balance by DID: %w", err)
-		}
-
-		if evType != nil {
-			passportScanEvent, err := EventsQ(r).
-				FilterByUserDID(req.UserDID).
-				FilterByType(evtypes.TypePassportScan).
-				FilterByStatus(data.EventOpen).
-				Get()
+	if !balance.PassportHash.Valid {
+		err = EventsQ(r).Transaction(func() error {
+			// If you make this endpoint public, you should check the passport hash for
+			// uniqueness and provide a better validation. Think about other changes too.
+			err = BalancesQ(r).FilterByDID(req.UserDID).SetPassport(req.Hash, expireDate)
 			if err != nil {
-				return fmt.Errorf("get passport_scan event by DID: %w", err)
+				return fmt.Errorf("set passport for balance by DID: %w", err)
 			}
-			logMsgOpenE := "PassportScan event not open"
-			if passportScanEvent != nil {
-				_, err = EventsQ(r).
+
+			if evType != nil {
+				passportScanEvent, err := EventsQ(r).
 					FilterByUserDID(req.UserDID).
 					FilterByType(evtypes.TypePassportScan).
-					Update(data.EventFulfilled, json.RawMessage(passportScanEvent.Meta), &reward)
+					FilterByStatus(data.EventOpen).
+					Get()
 				if err != nil {
-					return fmt.Errorf("update reward for passport_scan event by DID: %w", err)
+					return fmt.Errorf("get passport_scan event by DID: %w", err)
 				}
-				logMsgOpenE = "PassportScan event open"
-				logMsgScan = "PassportScan event reward update successful"
+				logMsgOpenE := "PassportScan event not open"
+				if passportScanEvent != nil {
+					_, err = EventsQ(r).
+						FilterByUserDID(req.UserDID).
+						FilterByType(evtypes.TypePassportScan).
+						Update(data.EventFulfilled, json.RawMessage(passportScanEvent.Meta), &reward)
+					if err != nil {
+						return fmt.Errorf("update reward for passport_scan event by DID: %w", err)
+					}
+					logMsgOpenE = "PassportScan event open"
+					logMsgScan = "PassportScan event reward update successful"
+				}
+				log.Debug(logMsgOpenE)
 			}
-			log.Debug(logMsgOpenE)
-		}
-		log.Debug(logMsgScan)
+			log.Debug(logMsgScan)
 
-		evType = EventTypes(r).Get(evtypes.TypeReferralSpecific, evtypes.FilterInactive)
-		if evType == nil {
-			log.Debug("Referral event type is disabled or expired, not accruing points to referrer")
+			evType = EventTypes(r).Get(evtypes.TypeReferralSpecific, evtypes.FilterInactive)
+			if evType == nil {
+				log.Debug("Referral event type is disabled or expired, not accruing points to referrer")
+				return nil
+			}
+
+			refDID := balance.ReferredBy.String
+			if refDID == "" {
+				return nil
+			}
+
+			err = EventsQ(r).Insert(data.Event{
+				UserDID: refDID,
+				Type:    evType.Name,
+				Status:  data.EventFulfilled,
+				Meta:    data.Jsonb(fmt.Sprintf(`{"did": "%s"}`, req.UserDID)),
+			})
+			if err != nil {
+				return fmt.Errorf("add event for referrer: %w", err)
+			}
+
 			return nil
-		}
-
-		refDID := balance.ReferredBy.String
-		if refDID == "" {
-			return nil
-		}
-
-		err = EventsQ(r).Insert(data.Event{
-			UserDID: refDID,
-			Type:    evType.Name,
-			Status:  data.EventFulfilled,
-			Meta:    data.Jsonb(fmt.Sprintf(`{"did": "%s"}`, req.UserDID)),
 		})
+
 		if err != nil {
-			return fmt.Errorf("add event for referrer: %w", err)
+			log.WithError(err).Error("Failed to set passport and add event for referrer")
+			ape.RenderErr(w, problems.InternalError())
+			return
 		}
 
-		return nil
-	})
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
 
+	if balance.PassportHash.String != req.Hash {
+		log.Error("passport_hash already in use")
+		ape.RenderErr(w, problems.Conflict())
+		return
+	}
+
+	err = BalancesQ(r).FilterByDID(req.UserDID).SetPassport(balance.PassportHash.String, expireDate)
 	if err != nil {
-		log.WithError(err).Error("Failed to set passport and add event for referrer")
+		log.WithError(err).Error("Failed to set expiration date")
 		ape.RenderErr(w, problems.InternalError())
 		return
 	}
