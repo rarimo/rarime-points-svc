16afe021803362ee74fde5f4273694fcc11a1267
Zaptoss <zaptosgg@gmail.com>
2024-05-23 21:22:02 +0300

    Refactor withdrawals code

--- internal/service/requests/withdraw.go
+++ internal/service/requests/withdraw.go
@@ -3,6 +3,7 @@ package requests
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
@@ -10,15 +11,18 @@ import (
 )
 
 func NewWithdraw(r *http.Request) (req resources.WithdrawRequest, err error) {
-	nullifier := chi.URLParam(r, "nullifier")
-
 	if err = json.NewDecoder(r.Body).Decode(&req); err != nil {
 		err = newDecodeError("body", err)
 		return
 	}
 
+	req.Data.ID = strings.ToLower(req.Data.ID)
+
 	return req, validation.Errors{
-		"data/id":                 validation.Validate(req.Data.ID, validation.Required, validation.In(nullifier)),
+		"data/id": validation.Validate(req.Data.ID,
+			validation.Required,
+			validation.In(strings.ToLower(chi.URLParam(r, "nullifier"))),
+			validation.Match(nullifierRegexp)),
 		"data/type":               validation.Validate(req.Data.Type, validation.Required, validation.In(resources.WITHDRAW)),
 		"data/attributes/amount":  validation.Validate(req.Data.Attributes.Amount, validation.Required, validation.Min(1)),
 		"data/attributes/address": validation.Validate(req.Data.Attributes.Address, validation.Required),
--- resources/model_withdraw_attributes.go
+++ resources/model_withdraw_attributes.go
@@ -4,9 +4,13 @@
 
 package resources
 
+import "encoding/json"
+
 type WithdrawAttributes struct {
 	// Rarimo address to withdraw to. Can be any valid address.
 	Address string `json:"address"`
 	// Amount of points to withdraw
 	Amount int64 `json:"amount"`
+	// JSON encoded ZK passport verification proof.
+	Proof json.RawMessage `json:"proof"`
 }
