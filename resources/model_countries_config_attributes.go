e50858465dd425d8a61330768b6a61c0c5d1b017
Zaptoss <zaptosgg@gmail.com>
2024-06-11 15:20:39 +0300

    Replace country map to country array

--- resources/model_countries_config_attributes.go
+++ resources/model_countries_config_attributes.go
@@ -6,5 +6,5 @@ package resources
 
 type CountriesConfigAttributes struct {
 	// Map from country codes to their properties. Each code is ISO 3166-1 alpha-3 code (3-letter uppercase).
-	Countries map[string]CountryProperties `json:"countries"`
+	Countries []CountryProperties `json:"countries"`
 }
--- resources/model_country_properties.go
+++ resources/model_country_properties.go
@@ -5,6 +5,8 @@
 package resources
 
 type CountryProperties struct {
+	// ISO 3166-1 alpha-3 country code
+	Code string `json:"code"`
 	// Whether the users of country are allowed to reserve (claim) tokens
 	ReserveAllowed bool `json:"reserve_allowed"`
 	// Whether the users of country are allowed to withdraw tokens
