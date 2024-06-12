1874c43b8d2cc6703a5c1aae3297c6fbdfa3b194
violog <51th.apprent1ce.f0rce@gmail.com>
2024-06-12 14:50:04 +0300

    Drop balance activation endpoint ,disable PoH runner again

--- internal/service/router.go
+++ internal/service/router.go
@@ -30,7 +30,6 @@ func Run(ctx context.Context, cfg config.Config) {
 			r.Route("/balances/{nullifier}", func(r chi.Router) {
 				r.Use(handlers.AuthMiddleware(cfg.Auth(), cfg.Log()))
 				r.Get("/", handlers.GetBalance)
-				r.Patch("/", handlers.ActivateBalance)
 				r.Post("/verifypassport", handlers.VerifyPassport)
 				r.Get("/withdrawals", handlers.ListWithdrawals)
 				r.Post("/withdrawals", handlers.Withdraw)
