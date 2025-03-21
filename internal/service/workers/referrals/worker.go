55aebb06855c72bb14d6ea47e4f61990396c09ef
2pa-arch <2pa-arch@users.noreply.github.com>
2025-03-21 12:13:57 +0200

    Change GuestBalnce to WithoutPassportScanBalance

--- internal/service/workers/referrals/worker.go
+++ internal/service/workers/referrals/worker.go
@@ -20,8 +20,8 @@ type worker struct {
 }
 
 type referralPair struct {
-	GuestBalance *data.Balance
-	Referral     *data.Referral
+	WithoutPassportScanBalance *data.Balance
+	Referral                   *data.Referral
 }
 
 func newWorker(cfg config.Config, workerName string) *worker {
@@ -48,10 +48,10 @@ func (w *worker) job() error {
 		timeStamp += 1
 		if err := w.updateReferralPair(referralEntry, timeStamp); err != nil {
 			w.log.WithFields(logan.F{
-				"error":              err,
-				"guest_nullifier":    referralEntry.GuestBalance.Nullifier,
-				"referral_nullifier": referralEntry.Referral.Nullifier,
-				"referred_by":        referralEntry.GuestBalance.ReferredBy.String,
+				"error":                           err,
+				"without_passport_scan_nullifier": referralEntry.WithoutPassportScanBalance.Nullifier,
+				"referral_nullifier":              referralEntry.Referral.Nullifier,
+				"referred_by":                     referralEntry.WithoutPassportScanBalance.ReferredBy.String,
 			}).Error("failed update pairs")
 		}
 	}
@@ -63,35 +63,35 @@ func (w *worker) findReferralPairs() (referralPairList []referralPair, err error
 	w.rq = w.rq.New().WithStatus()
 	w.bq = w.bq.New().FilterDisabled().FilterByIsPassportProven()
 
-	guestBalances, err := w.bq.Select()
+	withoutPassportScanBalances, err := w.bq.Select()
 	if err != nil {
-		return referralPairList, errors.Wrap(err, "failed to select guest balances")
+		return referralPairList, errors.Wrap(err, "failed to select without passport scan balances")
 	}
 
-	for _, balance := range guestBalances {
+	for _, balance := range withoutPassportScanBalances {
 		if !balance.ReferredBy.Valid {
 			continue
 		}
 		referral, err := w.rq.Get(balance.ReferredBy.String)
 		if err != nil {
 			w.log.WithFields(logan.F{
-				"error":           err,
-				"referred_by":     balance.ReferredBy.String,
-				"guest_nullifier": balance.Nullifier,
+				"error":                           err,
+				"referred_by":                     balance.ReferredBy.String,
+				"without_passport_scan_nullifier": balance.Nullifier,
 			}).Error("failed get referrale by referred_by")
 			continue
 		}
 
 		if referral.Status == data.StatusConsumed && balance.CreatedAt <= int32(time.Now().Add(-w.exc.CodeLifetime*time.Second).Unix()) {
 			referralPairList = append(referralPairList, referralPair{
-				GuestBalance: &balance,
-				Referral:     referral,
+				WithoutPassportScanBalance: &balance,
+				Referral:                   referral,
 			})
 
 			w.log.WithFields(logan.F{
-				"code":               balance.ReferredBy.String,
-				"guest_nullifier":    balance.Nullifier,
-				"referral_nullifier": referral.Nullifier,
+				"code":                            balance.ReferredBy.String,
+				"without_passport_scan_nullifier": balance.Nullifier,
+				"referral_nullifier":              referral.Nullifier,
 			}).Info("new pair for change")
 		}
 	}
@@ -100,7 +100,7 @@ func (w *worker) findReferralPairs() (referralPairList []referralPair, err error
 }
 
 func (w *worker) updateReferralPair(referralEntry referralPair, timeStamp uint64) error {
-	if err := w.bq.New().FilterByNullifier(referralEntry.GuestBalance.Nullifier).Update(map[string]any{"referred_by": w.exc.Code}); err != nil {
+	if err := w.bq.New().FilterByNullifier(referralEntry.WithoutPassportScanBalance.Nullifier).Update(map[string]any{"referred_by": w.exc.Code}); err != nil {
 		return errors.Wrap(err, "failed change referred_by")
 	}
 	return w.rq.Transaction(func() error {
